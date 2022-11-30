c63e5b01970c08a7b01c39307ecad2d5c6710845
Dyanesh S <dyanesh003@gmail.com>
2022-11-30 09:29:39 +0530

    Update script.js

--- assignments/assignment2/script.js
+++ assignments/assignment2/script.js
@@ -1,17 +1,33 @@
-const form = document.querySelector("form"),
-        nextBtn = form.querySelector(".nextBtn"),
-        backBtn = form.querySelector(".backBtn"),
-        allInput = form.querySelectorAll(".first input");
+function ValidateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var x = document.getElementById("email").value;
+    if (!x.match(mailformat)) {
+        alert("You have entered an invalid email address!");
+        document.getElementById("email").focus()
+    }
+}
 
+function PhoneNumberCheck(){
+    var x = document.getElementById("tel").value;
+    console.log(x)
+    if (!(x.toString().length == 10)) {
+        alert("Please enter a valid phone number!")
+        document.getElementById("phonenumber").focus()
+    }
+}
 
-nextBtn.addEventListener("click", ()=> {
-    allInput.forEach(input => {
-        if(input.value != ""){
-            form.classList.add('secActive');
-        }else{
-            form.classList.remove('secActive');
+function NameCheck() {
+    var x = document.getElementById("name").value;
+    var y = x.split("")
+    let sp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+    var arrLen = sp.length;
+    for (var i = 0; i < arrLen; i++) {
+        if (y.includes(sp[i])) {
+            alert("Please enter a valid name!")
+            document.getElementById("name").focus()
+            break
         }
-    })
-})
+    }
 
-backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+
+}
